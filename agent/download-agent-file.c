484e4b8b76baf29b0c57e4ad44db205f7789aaec
min7.choi <min7.choi@samsung.com>
2016-05-13 14:35:13 +0900

    Fix Prevent in tizen 2.4
    Change-Id: Ia8893bece8723b974990c2a5df114f27502e2521
    Signed-off-by: min7.choi <min7.choi@samsung.com>

--- agent/download-agent-file.c
+++ agent/download-agent-file.c
@@ -497,6 +497,9 @@ da_ret_t start_file_writing(da_info_t *da_info)
 		file_info->file_path = strdup(file_path);
 		free(origin_path);
 		ret = __decide_file_path_for_resume(file_info);
+        if(file_info) {
+            ret = __decide_file_path_for_resume(file_info);
+        }
 	} else {
 		ret = __decide_file_path(da_info);
 	}
